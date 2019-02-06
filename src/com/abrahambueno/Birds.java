8b7bdaa278e94a53343ef56fa7155d5367038295
Abraham Bueno <abueno@localhost.localdomain>
2019-02-06 16:16:11 -0600

    Work in progress

--- src/com/abrahambueno/Birds.java
+++ src/com/abrahambueno/Birds.java
@@ -9,6 +9,22 @@ public class Birds extends Animals {
         this.year = year;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     @Override
     public String move() {
         return null;
