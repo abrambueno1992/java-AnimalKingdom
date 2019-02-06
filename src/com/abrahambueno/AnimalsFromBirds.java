52dc560d44ed564da67e34902383e98d82a04d57
Abraham Bueno <abueno@localhost.localdomain>
2019-02-06 17:33:39 -0600

    Work in progress

---
+++ src/com/abrahambueno/AnimalsFromBirds.java
@@ -0,0 +1,13 @@
+package com.abrahambueno;
+
+public class AnimalsFromBirds extends AbstractAnimals {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getYear() {
+        return 0;
+    }
+}
---
+++ src/com/abrahambueno/AnimalsFromFish.java
@@ -0,0 +1,13 @@
+package com.abrahambueno;
+
+public class AnimalsFromFish extends AbstractAnimals {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getYear() {
+        return 0;
+    }
+}
